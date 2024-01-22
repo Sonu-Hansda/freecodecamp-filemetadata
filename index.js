682faddbe2cf87e3e016a6b85f839138730b27e2
Sonu-Hansda <sonukumarhansda61@gmmail.com>
2024-01-22 11:01:50 +0530

    Added solution

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -8,11 +10,15 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const fileName = req.file.originalname;
+  const fileType = req.file.mimetype;
+  const fileSize = req.file.size;
+  res.json({name: fileName, type: fileType, size: fileSize})
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
